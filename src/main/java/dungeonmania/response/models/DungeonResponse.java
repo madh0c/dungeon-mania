c90319c41ded7b90203b0b6ea82bb07a80a39fcb
Yang Hu <yangghu@icloud.com>
2021-10-29 17:22:37 +1100

    equals

--- src/main/java/dungeonmania/response/models/DungeonResponse.java
+++ src/main/java/dungeonmania/response/models/DungeonResponse.java
@@ -56,4 +56,77 @@ public final class DungeonResponse {
     public final List<EntityResponse> getEntities() {
         return entities;
     }
+
+    @Override
+    public boolean equals (Object obj) {
+        if (this == null || obj == null) {
+            return false;
+        }
+
+        if (this == obj) {
+            return true;
+        } 
+
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        DungeonResponse other = (DungeonResponse) obj;
+        if (dungeonId == null) {
+            if (other.dungeonId != null) {
+                return false;
+            }
+        } else if (!dungeonId.equals(other.dungeonId)) {
+            return false;
+        }
+
+        if (dungeonName == null) {
+            if (other.dungeonName != null) {
+                return false;
+            }
+        } else if (!dungeonName.equals(other.dungeonName)) {
+            return false;
+        }
+
+        if (entities == null) {
+            if (other.entities != null) {
+                return false;
+            }
+        } else if (!entities.equals(other.entities)) {
+            return false;
+        }
+
+        if (inventory == null) {
+            if (other.inventory != null) {
+                return false;
+            }
+        } else if (!inventory.equals(other.inventory)) {
+            return false;
+        }
+
+        if (buildables == null) {
+            if (other.buildables != null) {
+                return false;
+            }
+        } else if (!buildables.equals(other.buildables)) {
+            return false;
+        }
+
+        if (goals == null) {
+            if (other.goals != null) {
+                return false;
+            }
+        } else if (!goals.equals(other.goals)) {
+            return false;
+        }
+
+        if (animations == null) {
+            if (other.animations != null) {
+                return false;
+            }
+        } else if (!animations.equals(other.animations)) {
+            return false;
+        }
+
+        return true;
+    }
 }
