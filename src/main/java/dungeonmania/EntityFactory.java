b7f6537e14ddca2c5ab8fb7b293c72f7b72b78a5
Mark Huang <mark.huang@student.unsw.edu.au>
2021-10-29 17:41:51 +1100

    EntityFactory.java updated to have .contains

--- src/main/java/dungeonmania/EntityFactory.java
+++ src/main/java/dungeonmania/EntityFactory.java
@@ -4,46 +4,45 @@ import dungeonmania.allEntities.*;
 import dungeonmania.util.*;
 
 public class EntityFactory {
-	public static Entity createEntity(String type, Position position) {
-		if (type.equals("wall"))
+	public Entity createEntity(String type, Position position) {
+		if (type.contains("wall"))
 			return new Wall(position);
-		else if (type.equals("exit"))
+		else if (type.contains("exit"))
 			return new Exit(position);
-		else if (type.equals("boulder"))
+		else if (type.contains("boulder"))
 			return new Boulder(position);	
-		else if (type.equals("switch"))
+		else if (type.contains("switch"))
 			return new Switch(position);
-		else if (type.equals("door"))
+		else if (type.contains("Door"))
 			return new Door(position);
-		else if (type.equals("zombie_toast_spawner"))
+		else if (type.contains("zombie_toast_spawner"))
 			return new ZombieToastSpawner(position);
-		else if (type.equals("spider"))
+		else if (type.contains("spider"))
 			return new Spider(position);
-		else if (type.equals("zombie_toast"))
+		else if (type.contains("zombie_toast"))
 			return new ZombieToast(position);
-		else if (type.equals("mercenary"))
+		else if (type.contains("mercenary"))
 			return new Mercenary(position);
-		else if (type.equals("treasure"))
+		else if (type.contains("treasure"))
 			return new Treasure(position);
-		else if (type.equals("key"))
+		else if (type.contains("key"))
 			return new Key(position);
-		else if (type.equals("health_potion"))
+		else if (type.contains("health_potion"))
 			return new HealthPotion(position);
-		else if (type.equals("invincibility_potion"))
+		else if (type.contains("invincibility_potion"))
 			return new InvincibilityPotion(position);
-		else if (type.equals("invisibility_potion"))
+		else if (type.contains("invisibility_potion"))
 			return new InvisibilityPotion(position);
-		else if (type.equals("wood"))
+		else if (type.contains("wood"))
 			return new Wood(position);
-		else if (type.equals("arrow"))
+		else if (type.contains("arrow"))
 			return new Arrow(position);
-		else if (type.equals("bomb"))
+		else if (type.contains("bomb"))
 			return new Bomb(position);
-		else if (type.equals("sword"))
+		else if (type.contains("sword"))
 			return new Sword(position);
-		else if (type.equals("player"))
+		else if (type.contains("player"))
 			return new Player(position);
-
 		return null;
 	}
 }
