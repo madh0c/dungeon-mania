13471978d96b272893ef076c470a9ebf0df6b8b1
Jeffery Pan <z5310210@ad.unsw.edu.au>
2021-10-30 21:22:43 +1100

    cleaned up basicTest

--- src/test/java/dungeonmania/BasicTest.java
+++ src/test/java/dungeonmania/BasicTest.java
@@ -25,32 +25,6 @@ public class BasicTest {
         DungeonManiaController controller = new DungeonManiaController();
         assertDoesNotThrow(() -> controller.newGame("portals.json", "Standard"));
 
-        DungeonResponse dungeonInfo = controller.getDungeonInfo(0);
-
-        List<EntityResponse> entitiesInfo = dungeonInfo.getEntities(); 
-
-        assertEquals("0", dungeonInfo.getDungeonId()); 
-        assertEquals("portals.json", dungeonInfo.getDungeonName()); 
-
-        assertEquals(entitiesInfo.get(0).getId(), "0");
-        assertEquals(entitiesInfo.get(0).getType(), "player");
-        assertEquals(entitiesInfo.get(0).getPosition(), new Position(0, 0));
-        assertEquals(entitiesInfo.get(0).isInteractable(), true);
-
-        assertEquals(entitiesInfo.get(1).getId(), "1");
-        assertEquals(entitiesInfo.get(1).getType(), "portal");
-        assertEquals(entitiesInfo.get(1).getPosition(), new Position(1, 0));
-        assertEquals(entitiesInfo.get(1).isInteractable(), false);
-        
-        assertEquals(entitiesInfo.get(2).getId(), "2");
-        assertEquals(entitiesInfo.get(2).getType(), "portal");
-        assertEquals(entitiesInfo.get(2).getPosition(), new Position(4, 0));
-        assertEquals(entitiesInfo.get(2).isInteractable(), false);
-
-        assertEquals(new ArrayList<ItemResponse>(), dungeonInfo.getInventory());
-        assertEquals(new ArrayList<String>(), dungeonInfo.getBuildables());
-        assertEquals("enemies AND treasure", dungeonInfo.getGoals());      
-        
         List<EntityResponse> entityListCheck = new ArrayList<EntityResponse>();
 
         entityListCheck.add(new EntityResponse("0", "player", new Position(0, 0), true));
@@ -74,35 +48,6 @@ public class BasicTest {
 		DungeonManiaController controller = new DungeonManiaController();
         assertDoesNotThrow(() -> controller.newGame("goals.json", "Standard"));
 		assertDoesNotThrow(() -> controller.newGame("portals.json", "Standard"));
-
-        DungeonResponse dungeonInfo = controller.getDungeonInfo(0);
-		DungeonResponse dungeonInfo1 = controller.getDungeonInfo(1);
-
-        List<EntityResponse> entitiesInfo = dungeonInfo.getEntities(); 
-
-        assertEquals("0", dungeonInfo.getDungeonId()); 
-        assertEquals("goals.json", dungeonInfo.getDungeonName()); 
-		assertEquals("1", dungeonInfo1.getDungeonId());
-		assertEquals("portals.json", dungeonInfo1.getDungeonName()); 
-
-        assertEquals(entitiesInfo.get(0).getId(), "0");
-        assertEquals(entitiesInfo.get(0).getType(), "player");
-        assertEquals(entitiesInfo.get(0).getPosition(), new Position(0, 0));
-        assertEquals(entitiesInfo.get(0).isInteractable(), true);
-
-        assertEquals(entitiesInfo.get(1).getId(), "1");
-        assertEquals(entitiesInfo.get(1).getType(), "boulder");
-        assertEquals(entitiesInfo.get(1).getPosition(), new Position(2, 1));
-        assertEquals(entitiesInfo.get(1).isInteractable(), true);
-        
-        assertEquals(entitiesInfo.get(2).getId(), "2");
-        assertEquals(entitiesInfo.get(2).getType(), "bomb");
-        assertEquals(entitiesInfo.get(2).getPosition(), new Position(3, 4));
-        assertEquals(entitiesInfo.get(2).isInteractable(), true);
-
-        assertEquals(new ArrayList<ItemResponse>(), dungeonInfo.getInventory());
-        assertEquals(new ArrayList<String>(), dungeonInfo.getBuildables());
-        assertEquals("enemies AND (treasure OR (exit AND (bomb OR boulder)))", dungeonInfo.getGoals());      
         
         List<EntityResponse> entityListCheck = new ArrayList<EntityResponse>();
 
