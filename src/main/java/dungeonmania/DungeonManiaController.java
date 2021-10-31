1ebc6cd752db28b947d53b11d59f0056f9f11e08
Hua Hu <z5313381@ad.unsw.edu.au>
2021-10-31 11:54:29 +1100

    setter

--- src/main/java/dungeonmania/DungeonManiaController.java
+++ src/main/java/dungeonmania/DungeonManiaController.java
@@ -154,12 +154,6 @@ public class DungeonManiaController {
 	}
 
 	public DungeonResponse saveGame(String name) throws IllegalArgumentException {
-		String dungeonIdInt = 
-		for (Dungeon dungeon : games) {
-			if (dungeon.getId() == name) {
-				target = dungeon;
-			}
-		}
 		return null;
     }
 
