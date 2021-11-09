7fbb8345af3e36df86e06370020fd4b47dbec78b
Jeffery Pan <z5310210@ad.unsw.edu.au>
2021-11-09 12:57:32 +1100

    assassin implemented

--- src/main/java/dungeonmania/allEntities/Assassin.java
+++ src/main/java/dungeonmania/allEntities/Assassin.java
@@ -7,7 +7,7 @@ public class Assassin extends Mercenary {
 
 	public Assassin (String id, Position position) {
 		super(id, position, true);
-		super.setType("assasin");
+		super.setType("assassin");
 		super.setHealth(30);
 		super.setBaseAttack(20);
 	}
