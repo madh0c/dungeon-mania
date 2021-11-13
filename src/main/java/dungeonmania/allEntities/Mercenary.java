79eccb66d6ddae103b720b3f7479d637e001ae54
Hua Hu <z5313381@ad.unsw.edu.au>
2021-11-13 20:57:07 +1100

    refactor merc

--- src/main/java/dungeonmania/allEntities/Mercenary.java
+++ src/main/java/dungeonmania/allEntities/Mercenary.java
@@ -169,37 +169,15 @@ public class Mercenary extends MovingEntity {
 			
 		}
 	}
-	
-	public boolean moveDumb(Direction direction, Dungeon dungeon) {
-		// Check if collidable with next entity
-		Position pos = getPosition();
-		Entity ent = dungeon.getEntity(pos.translateBy(direction));
-		Direction prevDir = getCurrentDir();
-		setCurrentDir(direction);
-		if (ent != null && !collide(ent, dungeon)) {
-			setCurrentDir(prevDir);
-			return false;
-		}
-
-		setPosition(getPosition().translateBy(direction));
-
-		portalMove(dungeon, pos);
-		return true;
-	}
-
 
 	/**
-	 * Move the mercenary towards the player using Dijkstra's algorithm <p>
-	 * @param dungeon Current dungeon of mercenary
+	 * Move the mercenery iff it can move to a cardinally adjacent cell that is closer to the player than
+	 * the current cell.
+	 * @param direction	Desired direction of mercenary
+	 * @param dungeon	Current dungeon of mercenary
+	 * @return boolean of whether the mercenary got moved
 	 */
-	@Override
-	public void move(Dungeon dungeon) {
-		// if stuck in swamp
-		if (super.getTicksFrozen() > 0) {
-			super.setTicksFrozen(super.getTicksFrozen() - 1);
-			return;
-		}
-
+	public boolean dijkstraMove(Dungeon dungeon) {		
 		Position currPos = getPosition();
 		
 		Position nextPos = null;
@@ -225,8 +203,57 @@ public class Mercenary extends MovingEntity {
 			if (!isAlly) {
 				Battle.battle(this, dungeon);
 			}
-		}
+		} return true;
+	}
+	
+	public boolean moveDumb(Direction direction, Dungeon dungeon) {
+		// Check if collidable with next entity
+		Position pos = getPosition();
+		Entity ent = dungeon.getEntity(pos.translateBy(direction));
+		Direction prevDir = getCurrentDir();
+		setCurrentDir(direction);
+		if (ent != null && !collide(ent, dungeon)) {
+			setCurrentDir(prevDir);
+			return false;
+		} setPosition(getPosition().translateBy(direction));
 
+		portalMove(dungeon, pos);
+		return true;
+	}
+
+
+
+	/**
+	 * Move the mercenary towards the player<p>
+	 * Up / Down first priority<p>
+	 * Left / Right second priority
+	 * @param dungeon	Current dungeon of mercenary
+	 */
+	@Override
+	public void move(Dungeon dungeon) {
+		// if stuck in swamp
+		if (super.getTicksFrozen() > 0) {
+			super.setTicksFrozen(super.getTicksFrozen() - 1);
+			return;
+		}
+		// Find player
+		Player player = dungeon.getPlayer();
+		if (player == null) {
+			return;
+		}
+		
+		// Move towards the player using Dijkstra's algorithm
+		if (player.getPosition().getY() != getPosition().getY()) {
+			dijkstraMove(dungeon);
+		} else if (player.getPosition().getX() != getPosition().getX()) {
+			dijkstraMove(dungeon);
+		} else if (player.getPosition() == getPosition()) {
+			if (!isAlly) {
+				if (enemyAttack()) {
+					Battle.battle(this, dungeon);
+				}
+			}
+		}
 	}
 	
 	@Override
@@ -242,29 +269,21 @@ public class Mercenary extends MovingEntity {
 		if (player.getPosition().getY() != getPosition().getY()) {
 			// If player is to the up of merc
 			if (player.getPosition().getY() < getPosition().getY()) {
-				if(moveDumb(Direction.DOWN, dungeon)) {
+				if (moveDumb(Direction.DOWN, dungeon)) {
 					return;
 				}
-				
-			} 
-			// If on down side
-			else {
+			} else {
 				if (moveDumb(Direction.UP, dungeon)) {
 					return;
 				}
 			}
-		}
-
-		// left right movement
-		if (player.getPosition().getX() != getPosition().getX()) {
+		} else if (player.getPosition().getX() != getPosition().getX()) {
 			// If player is to the left of merc
 			if (player.getPosition().getX() < getPosition().getX()) {
 				if (moveDumb(Direction.RIGHT, dungeon)) {
 					return;
 				}
-			} 
-			// If on right side
-			else {
+			}  else {
 				if (moveDumb(Direction.LEFT, dungeon)) {
 					return;
 				}
